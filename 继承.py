17a8ac66230ece886ae4e9041c204a5d1b27dd8d
lsy <2367860903@qq.com>
2020-09-27 22:11:16 +0800

    继承

---
+++ 继承.py
@@ -0,0 +1,17 @@
+class Mammal:
+    def work(seld):
+        print("Work!")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("bark")
+
+
+class Cat(Mammal):
+    pass
+
+
+dogone = Dog()
+dogone.work()
+dogone.bark()
